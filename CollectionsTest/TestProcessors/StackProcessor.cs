485ce42ba08e6a8bd52d029dc38d4f050aeea85f
polindrom <autocrossauto@gmail.com>
2014-07-13 21:07:57 +0300

    minor changes (deleted unused "using")

--- CollectionsTest/TestProcessors/StackProcessor.cs
+++ CollectionsTest/TestProcessors/StackProcessor.cs
@@ -1,9 +1,6 @@
 ﻿using CollectionsTest.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CollectionsTest.TestProcessors
 {
